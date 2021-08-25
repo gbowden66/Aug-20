64c518a2f2da4bf385c64127981e8de13bf4a3eb
gbowden66 <89275329+gbowden66@users.noreply.github.com>
2021-08-25 15:33:40 -0400

    Add files via upload

---
+++ AUG23.R
@@ -0,0 +1,6 @@
+#author Gary, aug 23, 2021, purpose: calculating correlation coefficient
+
+if(!require(devtools))install.packages("devtools")devtools::install_github("kassambara/ggpubr")
+
+library("ggpubr")
+
