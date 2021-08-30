f33f255f252b0fa44ca562bf441e6b23db12e4e1
gbowden66 <89275329+gbowden66@users.noreply.github.com>
2021-08-30 15:10:36 -0400

    Add files via upload

---
+++ AUG16.R
@@ -0,0 +1,17 @@
+#author: Gary Bowden, Date: Aug 16, 2021, Purpose: Calculate stdev, median values
+
+#calculate standard deviation and standard deviation.
+
+#a variable to hold 7 numbers.
+
+variable1 <- c(1,2,3,4,5,6,7)
+
+sd(variable1)
+
+#2.160247
+
+#calculate median.
+
+median(variable1)
+
+#4
