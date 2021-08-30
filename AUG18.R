fb51bbafe27eee5ddc372eb0addad7fc6e8cda47
gbowden66 <89275329+gbowden66@users.noreply.github.com>
2021-08-30 15:17:38 -0400

    Add files via upload

---
+++ AUG18.R
@@ -0,0 +1,25 @@
+#author: Gary, date: aug 18, 2021, purpose: calculate mean, median, mode
+
+#dummy data
+
+variable1 <- c(1,2,3,4,5)
+
+#calculate the mean/average of variable1
+
+mean <- mean(variable1)
+#3
+
+#calculate median value
+
+median <- median(variable1)
+#3
+
+#calculate mode value
+
+variable2 <- c(1,2,3,4,5,2)
+#unimodal data, hust one which is "2"
+
+#install the external packages in R
+install.packages("modeest")
+
+#Please excuse the late submission on R documents of Aug 16 and 18. I was in the process of moving and did not have my computer set up with the proper applications to complete this work. Thank you for your understanding.
